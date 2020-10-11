76fa070a99618d18cb8c59037429c83785528d1a
Stavros Vergos <stavrospvergos@gmail.com>
2020-10-11 18:53:31 +0300

    ingredient sum calculating

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,10 +9,10 @@ class BurgerBuilder extends Component {
     // }
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            salad: 0,
+            bacon: 0,
+            cheese: 1,
+            meat: 1
         }
     }
 
