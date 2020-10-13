a10c9059ffffb9cde99364f8a12f0fbc928397aa
Stavros Vergos <stavrospvergos@gmail.com>
2020-10-14 02:15:16 +0300

    order summary modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+
+const orderSummary = (props) => {
+
+    const ingredientSummary = Object.keys(props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                    </li>);
+            });
+
+    return (
+        <Fragment>
+            <h3>Your Order</h3>
+            <p>An excellent choice with:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </Fragment>
+    )
+};
+
+export default orderSummary;
