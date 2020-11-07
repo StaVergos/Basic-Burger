ace2dda939979d19fdc6fb870368abb42ad62349
Stavros Vergos <stavrospvergos@gmail.com>
2020-11-07 03:49:00 +0200

    navigating checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,28 +94,29 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert('You continue!');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Kalamitis',
-                address: {
-                    street: 'Kalamos',
-                    zipCode: '80100',
-                    country: 'Greece'
-                },
-                email: 'kalamos@kalamos.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Kalamitis',
+        //         address: {
+        //             street: 'Kalamos',
+        //             zipCode: '80100',
+        //             country: 'Greece'
+        //         },
+        //         email: 'kalamos@kalamos.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false});
+        //     });
+        this.props.history.push('/checkout');
     }
 
     render() {
