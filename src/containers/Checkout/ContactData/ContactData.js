053cf8ecc47befbd6c7432f1db514c429c285bce
Stavros Vergos <stavrospvergos@gmail.com>
2020-12-02 09:33:17 +0200

    combining reducers

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -89,7 +89,7 @@ class ContactData extends Component {
                     ],
                     config: {}
                 },
-                value: '',
+                value: 'fastest',
                 validation: {},
                 valid: true
             }
@@ -191,9 +191,9 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice,
-        loading: state.loading
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     };
 };
 
